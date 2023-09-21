e742cbeacc08bfdda0d58635c5bd81e7fecfdffa
sabifromtherock <szabolcs.sziklai@protonmail.com>
2023-09-21 16:52:30 +0100

    added Card class

---
+++ src/main/java/Card.java
@@ -0,0 +1,36 @@
+public class Card {
+  private String suit;
+  private String symbol;
+  private int value;
+  private static final String ANSI_RESET = "\033[1;37m";
+  private static final String ANSI_RED = "\033[1;31m";
+  private static final String ANSI_BLACK = "\033[1;30m";
+  private static final String ANSI_BACKGROUND = "\033[0;107m";
+
+  public Card(String suit, String symbol, int value) {
+    this.suit = suit;
+    this.symbol = symbol;
+    this.value = value;
+  }
+
+  public String getSuit() {
+    return suit;
+  }
+
+  public String getSymbol() {
+    return symbol;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  @Override
+  public String toString() {
+    if (suit.equals("♥") || suit.equals("♦")) {
+      return ANSI_BACKGROUND + ANSI_RED + symbol + " of " + suit + " (Value: " + value + ")" + ANSI_RESET;
+    } else {
+      return ANSI_BACKGROUND + ANSI_BLACK + symbol + " of " + suit + " (Value: " + value + ")" + ANSI_RESET;
+    }
+  }
+}
