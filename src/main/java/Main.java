3ccb9d7c253a475bd4cddf4446fa6916762c7eff
sabifromtherock <szabolcs.sziklai@protonmail.com>
2023-09-26 15:53:09 +0100

    added 2 players to the game

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    SnapGame snapGame = new SnapGame("Snap Game");
+    Player player1 = new Player("Bob");
+    Player player2 = new Player("John");
+    SnapGame snapGame = new SnapGame("Snap Game", player1, player2);
     snapGame.shuffleDeck();
     snapGame.playSnapGame();
   }
--- src/main/java/SnapGame.java
+++ src/main/java/SnapGame.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SnapGame extends CardGame {
+  private Player player1;
+  private Player player2;
+  boolean isTimeElapsed = false;
+  int currentPlayer = 1;
 
-  public SnapGame(String name) {
+  public SnapGame(String name, Player player1, Player player2) {
     super(name);
+    this.player1 = player1;
+    this.player2 = player2;
   }
 
   public void playSnapGame() {
@@ -21,16 +29,51 @@ public class SnapGame extends CardGame {
         break;
       }
 
-      System.out.println("You drew: " + currentCard);
+      if (currentPlayer == 1) {
+        System.out.println(Color.PLAYER1.getColor() + player1.getName() + " drew: " + currentCard);
+      } else {
+        System.out.println(Color.PLAYER2.getColor() + player2.getName() + " drew: " + currentCard);
+      }
 
       if (currentCard.getSymbol().equals(previousCardSymbol)) {
-        System.out.println(Color.GREEN.getColor() + "Snap! You won!");
-        break;
+        System.out.println();
+
+        // timer setup
+        Timer timer = new Timer();
+        TimerTask snapTask = new TimerTask() {
+          @Override
+          public void run() {
+            System.out.println(Color.RED.getColor() + "Time's up! " + Color.GREEN.getColor() + (currentPlayer == 1 ? player2.getName() : player1.getName()) + " wins.");
+            isTimeElapsed = true;
+            timer.cancel();
+          }
+        };
+        timer.schedule(snapTask, 4000);
+        
+        System.out.println(isTimeElapsed);
+        if (isTimeElapsed) {
+          System.out.println(Color.RED.getColor() + "Time's up! " + Color.GREEN.getColor() + (currentPlayer == 1 ? player2.getName() : player1.getName()) + " wins.");
+          break;
+        }
+
+        String userInput = scanner.nextLine();
+        timer.cancel();
+
+        if (userInput.equalsIgnoreCase("snap")) {
+          System.out.println(Color.GREEN.getColor() + "Snap! " + (currentPlayer == 1 ? player1.getName() : player2.getName()) + " wins!");
+          break;
+        } else {
+          System.out.println(Color.RED.getColor() + "You lost! " + Color.GREEN.getColor() + (currentPlayer == 1 ? player2.getName() : player1.getName()) + " wins!");
+          break;
+        }
       }
 
       previousCardSymbol = currentCard.getSymbol();
+      currentPlayer = (currentPlayer == 1) ? 2 : 1;
     }
 
     scanner.close();
   }
+
+
 }
